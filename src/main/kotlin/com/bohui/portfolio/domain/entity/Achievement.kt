126c0f510aab8e0a0c2f9897bd3a99957509be82
bh <lodin356@naver.com>
2024-10-13 14:09:19 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/bohui/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/bohui/portfolio/domain/entity/Achievement.kt
@@ -1,15 +1,30 @@
 package com.bohui.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
-class Achievement : BaseEntity() {
+class Achievement(
+    title: String,
+    description: String,
+    achievedDate: LocalDate?, //null ok
+    host: String,
+    isActive: Boolean //기본생성자
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "achievement_id")
     var id: Long? = null
-    // val achievement: Achi...
 
+    var title: String = title
+
+    var description: String = description
+
+    var achievedDate: LocalDate? = achievedDate
+
+    var host: String = host
+
+    var isActive: Boolean = isActive //다섯개의 필드
 
 }
--- src/main/kotlin/com/bohui/portfolio/domain/entity/Link.kt
+++ src/main/kotlin/com/bohui/portfolio/domain/entity/Link.kt
@@ -3,11 +3,21 @@ package com.bohui.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class Link : BaseEntity() {
+class Link(
+    name: String,
+    content: String,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "link_id")
     var id: Long? = null
 
+    var name: String = name
+
+    var content: String = content
+
+    // is_active 로 DB에 들어간다. (jpa에서 알아서 맵핑할 수 있도록)
+    var isActive: Boolean = isActive
 }
