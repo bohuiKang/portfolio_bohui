6cc30b19e6bd3efed02067725972a7b8e11a1d40
bh <lodin356@naver.com>
2024-10-15 01:49:34 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/bohui/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/bohui/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,13 +2,16 @@ package com.bohui.portfolio.domain.repository
 
 import com.bohui.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
 
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
 
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
     // 상위에 정의된 기능이여서 override를 해줘야한다.
     override fun findById(id: Long): Optional<Experience>
-}
 
+}
--- src/main/kotlin/com/bohui/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/bohui/portfolio/domain/repository/ProjectRepository.kt
@@ -2,12 +2,15 @@ package com.bohui.portfolio.domain.repository
 
 import com.bohui.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, Long> {
 
+    @Query("select p from Project p left join fetch p.skills s left join fetch s.skill where p.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Project>
 
+    @Query("select p from Project p join fetch p.details where p.id = :id")
     // 상위에 정의된 기능이여서 override를 해줘야한다.
     override fun findById(id: Long): Optional<Project>
 }
