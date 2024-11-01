7fea5b2004bfa1c4405bbc48d9ab697980adff65
bh <lodin356@naver.com>
2024-11-01 17:10:12 +0900

    인터셉터 개발

--- src/main/kotlin/com/bohui/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/bohui/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,25 @@
 package com.bohui.portfolio.presentation.interceptor
 
+import com.bohui.portfolio.domain.entity.HttpInterface
+import com.bohui.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+}
