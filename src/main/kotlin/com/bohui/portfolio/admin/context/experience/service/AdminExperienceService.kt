5e09ea4108afe7026433b510037004984d93f9b0
bh <lodin356@naver.com>
2024-11-08 18:53:57 +0900

    삽입, 수정, 조회 API 개발 - 연관관계 일대다

--- src/main/kotlin/com/bohui/portfolio/admin/context/experience/service/AdminExperienceService.kt
+++ src/main/kotlin/com/bohui/portfolio/admin/context/experience/service/AdminExperienceService.kt
@@ -1,10 +1,12 @@
 package com.bohui.portfolio.admin.context.experience.service
 
+import com.bohui.portfolio.admin.context.experience.form.ExperienceForm
 import com.bohui.portfolio.admin.data.TableDTO
 import com.bohui.portfolio.admin.exception.AdminBadRequestException
 import com.bohui.portfolio.domain.entity.Experience
 import com.bohui.portfolio.domain.entity.ExperienceDetail
 import com.bohui.portfolio.domain.repository.ExperienceRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
@@ -27,4 +29,46 @@ class AdminExperienceService(
 
         return TableDTO.from(classInfo, entities)
     }
+
+    @Transactional
+    fun save(form: ExperienceForm) {
+        val experienceDetails = form.details
+            ?.map { detail -> detail.toEntity() } // null이 아닐경우 map{}
+            ?.toMutableList() // null이 아닐경우  toMutableList()
+        val experience = form.toEntity()
+        experience.addDetails(experienceDetails)
+
+        experienceRepository.save(experience)
+    }
+
+    @Transactional
+    fun update(id: Long, form: ExperienceForm) {
+        val experience = experienceRepository.findById(id)
+            .orElseThrow { throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수 없습니 다.") }
+
+        experience.update(
+            title = form.title,
+            description = form.description,
+            startYear = form.startYear,
+            startMonth = form.startMonth,
+            endYear = form.endYear,
+            endMonth = form.endMonth,
+            isActive = form.isActive
+        )
+
+        val detailMap = experience.details.map { it.id to it }.toMap()
+
+        form.details?.forEach {
+            val entity = detailMap.get(it.id)
+            if (entity != null) {
+                entity.update(
+                    content = it.content,
+                    isActive = it.isActive
+                )
+            } else {
+                experience.details.add(it.toEntity())
+            }
+        }
+    }
+
 }
--- src/main/kotlin/com/bohui/portfolio/admin/context/project/service/AdminProjectService.kt
+++ src/main/kotlin/com/bohui/portfolio/admin/context/project/service/AdminProjectService.kt
@@ -1,13 +1,12 @@
 package com.bohui.portfolio.admin.context.project.service
 
+import com.bohui.portfolio.admin.context.project.form.ProjectForm
 import com.bohui.portfolio.admin.data.TableDTO
 import com.bohui.portfolio.admin.exception.AdminBadRequestException
-import com.bohui.portfolio.domain.entity.Experience
-import com.bohui.portfolio.domain.entity.ExperienceDetail
 import com.bohui.portfolio.domain.entity.Project
 import com.bohui.portfolio.domain.entity.ProjectDetail
-import com.bohui.portfolio.domain.repository.ExperienceRepository
 import com.bohui.portfolio.domain.repository.ProjectRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
@@ -29,5 +28,47 @@ class AdminProjectService(
             .details else emptyList()
 
         return TableDTO.from(classInfo, entities)
+    }
+
+    @Transactional
+    fun save(form: ProjectForm) {
+        val projectDetails = form.details
+            ?.map { detail -> detail.toEntity() } // null이 아닐경우 map{}
+            ?.toMutableList() // null이 아닐경우  toMutableList()
+        val project = form.toEntity()
+        project.addDetails(projectDetails)
+
+        projectRepository.save(project)
+    }
+
+    @Transactional
+    fun update(id: Long, form: ProjectForm) {
+        val project = projectRepository.findById(id)
+            .orElseThrow { throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수 없습니 다.") }
+
+        project.update(
+            name = form.name,
+            description = form.description,
+            startYear = form.startYear,
+            startMonth = form.startMonth,
+            endYear = form.endYear,
+            endMonth = form.endMonth,
+            isActive = form.isActive
+        )
+
+        val detailMap = project.details.map { it.id to it }.toMap()
+
+        form.details?.forEach {
+            val entity = detailMap.get(it.id)
+            if (entity != null) {
+                entity.update(
+                    content = it.content,
+                    url = it.url,
+                    isActive = it.isActive
+                )
+            } else {
+                project.details.add(it.toEntity())
+            }
+        }
     }
 }
